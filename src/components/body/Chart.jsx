f4edb78c1c66cddb181ddb77c009ebfcafce897c
ayodeji_ <ay.programmr@gmail.com>
2020-04-05 03:12:55 +0100

    Fixing Chart, created and populated json data file

--- src/components/body/Chart.jsx
+++ src/components/body/Chart.jsx
@@ -11,8 +11,8 @@ const chartConfigs = {
     type: 'area2d',// The chart type
     width: '100%', // Width of the chart
     height: '100%', // Height of the chart
-    dataFormat: 'json', // Data type
-    dataSource: '../../assets/resources/OilData.json',
+    dataFormat: 'jsonurl', // Data type
+    dataSource: 'https://github.com/ayodejipy/BANKWITHMINT-MERCH-DASHBOARD/blob/master/src/assets/resources/OilData.json',
 };
 
 //Your react component
